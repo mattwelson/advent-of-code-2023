5ddbfeeed4dba535d00d02ec5121cb60e377e50e
matt-welson <matt.welson@mtf.co.nz>
2023-12-05 15:11:50 +1300

    feat: day01

---
+++ src/day01/index.spec.ts
@@ -0,0 +1,51 @@
+import { describe, expect, it } from "vitest";
+
+import sampleData from "./sample.txt?raw";
+import sampleData2 from "./sample2.txt?raw";
+import data from "./data.txt?raw";
+import { parse, parseAlphaNumeric, sum } from ".";
+
+describe("problem 1 - sample data", () => {
+  it("imports and parses", () => {
+    expect(parse(sampleData)).toHaveLength(4);
+    expect(parse(sampleData)[0]).toBe(12);
+    expect(parse(sampleData)[1]).toBe(38);
+    expect(parse(sampleData)[2]).toBe(15);
+    expect(parse(sampleData)[3]).toBe(77);
+  });
+
+  it("sum", () => {
+    expect(sum(parse(sampleData))).toBe(142);
+  });
+});
+
+describe("problem 1 - data", () => {
+  it("sum", () => {
+    expect(sum(parse(data))).toBe(54159);
+  });
+});
+
+describe("problem 2 - sample data", () => {
+  it("parse", () => {
+    expect(parseAlphaNumeric(sampleData2)).toHaveLength(7);
+    expect(parseAlphaNumeric(sampleData2)[0]).toBe(29);
+    expect(parseAlphaNumeric(sampleData2)[1]).toBe(83);
+    expect(parseAlphaNumeric(sampleData2)[2]).toBe(13);
+    expect(parseAlphaNumeric(sampleData2)[3]).toBe(24);
+    expect(parseAlphaNumeric(sampleData2)[4]).toBe(42);
+    expect(parseAlphaNumeric(sampleData2)[5]).toBe(14);
+    expect(parseAlphaNumeric(sampleData2)[6]).toBe(76);
+  });
+
+  it("sum", () => {
+    expect(sum(parseAlphaNumeric(sampleData2))).toBe(281);
+  });
+});
+
+describe("problem 1 - sample data", () => {
+  it("sum", () => {
+    // Oopsy, wrong answer!
+    expect(sum(parseAlphaNumeric(data))).toBeGreaterThan(53859);
+    expect(sum(parseAlphaNumeric(data))).toBe(53866);
+  });
+});
---
+++ src/day01/index.ts
@@ -0,0 +1,43 @@
+export function parse(input: string) {
+  return input
+    .split("\r")
+    .map((l) => [...l].filter((c) => /\d/.test(c)))
+    .map((l) => `${l[0]}${l[l.length - 1]}`)
+    .map((n) => parseInt(n, 10));
+}
+
+export function sum(input: number[]) {
+  return input.reduce((acc, curr) => acc + curr, 0);
+}
+
+const allNumbers = [
+  { text: "one", value: 1 },
+  { text: "two", value: 2 },
+  { text: "three", value: 3 },
+  { text: "four", value: 4 },
+  { text: "five", value: 5 },
+  { text: "six", value: 6 },
+  { text: "seven", value: 7 },
+  { text: "eight", value: 8 },
+  { text: "nine", value: 9 },
+];
+
+// Problem 2 is quite different, so instead I've inverted the way we're parsing the text
+export function parseAlphaNumeric(input: string) {
+  return input
+    .split("\r")
+    .map((l) =>
+      [...l]
+        .reduce((acc, curr, i) => {
+          let n = parseInt(curr, 10);
+          if (!Number.isNaN(n)) return [...acc, n];
+          let subString = l.slice(i, i + 6);
+          let alpha = allNumbers.find(({ text }) => subString.startsWith(text));
+          if (alpha) return [...acc, alpha.value];
+          return acc;
+        }, [] as number[])
+        .join("")
+    )
+    .map((l) => `${l[0]}${l[l.length - 1]}`)
+    .map((n) => parseInt(n, 10));
+}
